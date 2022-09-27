6a4ddf7333e623d8885f3192c4ca1d28098e26eb
Derek Gilmore <dgilmore@cloudflare.com>
2022-09-27 12:26:20 -0500

    Initial spike

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
-export default {
-	fetch() {
-		return new Response('Hello worker!', {
-			headers: {
-				'content-type': 'text/plain',
-			},
-		});
-	},
+// index.js
+var my_code_club_default = {
+   fetch(request) {
+       //if(request.url == "https://code-club.dgilmore-lab.workers.dev/") {
+       //    return new Response('Hello worker!', {
+       //        headers: {
+       //            'content-type': 'text/plain',
+       //        },
+       //    });
+       //}
+       if(request.method == 'POST') {
+           return new Response('Hello worker! Method: POST', {
+               headers: {
+                   'content-type': 'text/plain',
+               },
+           });
+       }
+       else{
+           return new Response('Error Worker!', {
+               headers: {
+                   'content-type': 'text/plain',
+               },
+           });
+       }
+   },
 };
+
+export {
+  my_code_club_default as default
+};
+//# sourceMappingURL=index.js.map
